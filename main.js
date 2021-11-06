2a7b9b9171b6dc2dd9856ff9ac4796e4839d0068
simmax4you <93457125+simmax4you@users.noreply.github.com>
2021-11-06 11:20:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function song1(){
+    document.getElementById("audio1").play();
+}
+function song2(){
+    document.getElementById("audio2").play();
+}
+function song3(){
+    document.getElementById("audio3").play();
+}
+function song4(){
+    document.getElementById("audio4").play();
+}
